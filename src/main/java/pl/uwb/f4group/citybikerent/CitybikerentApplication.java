a3364a0feb153de4897c2d9bf47f21c68310b551
Marcin <Cinuu0410@gmail.com>
2023-12-06 14:56:55 +0100

    initial commit

---
+++ src/main/java/pl/uwb/f4group/citybikerent/CitybikerentApplication.java
@@ -0,0 +1,13 @@
+package pl.uwb.f4group.citybikerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitybikerentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitybikerentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/uwb/f4group/citybikerent/CitybikerentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uwb.f4group.citybikerent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybikerentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
