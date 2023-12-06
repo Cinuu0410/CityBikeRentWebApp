f38f92d6c9d7c796d5629d8b7bd29b7ac35cd52f
Szałaj <szalajdaniel@interia.pl>
2023-12-06 18:18:25 +0100

    test hejka

--- src/main/java/pl/uwb/f4group/citybikerent/CityBikeRentApplication.java
+++ src/main/java/pl/uwb/f4group/citybikerent/CityBikeRentApplication.java
@@ -9,6 +9,7 @@ public class CityBikeRentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CityBikeRentApplication.class, args);
 		System.out.println("hello");
+		System.out.println("hejka");
 	}
 
 }
