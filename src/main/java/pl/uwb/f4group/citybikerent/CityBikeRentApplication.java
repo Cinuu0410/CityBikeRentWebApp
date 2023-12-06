1b9494577655643793d6fbc1804aaeb1a874525e
Marcin <cinuu0410@gmail.com>
2023-12-06 17:52:35 +0100

    renamed classs test

--- src/main/java/pl/uwb/f4group/citybikerent/CityBikeRentApplication.java
+++ src/main/java/pl/uwb/f4group/citybikerent/CityBikeRentApplication.java
@@ -8,6 +8,7 @@ public class CityBikeRentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CityBikeRentApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
