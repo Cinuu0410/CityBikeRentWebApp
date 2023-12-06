cf176a9063da1fd3b20889c2e01aac09f845c4b1
Marcin <Cinuu0410@gmail.com>
2023-12-06 17:48:28 +0100

    renamed class

--- src/main/java/pl/uwb/f4group/citybikerent/CityBikeRentApplication.java
+++ src/main/java/pl/uwb/f4group/citybikerent/CityBikeRentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CitybikerentApplication {
+public class CityBikeRentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CitybikerentApplication.class, args);
+		SpringApplication.run(CityBikeRentApplication.class, args);
 	}
 
 }
